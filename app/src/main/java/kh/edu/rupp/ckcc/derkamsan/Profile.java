cfb813146f81c960f10450bfcd5acd11ceceaee8
Sokheang <sokheangchhoem0408@gmail.com>
2018-12-26 16:59:06 +0700

    my commit

---
+++ app/src/main/java/kh/edu/rupp/ckcc/derkamsan/Profile.java
@@ -0,0 +1,50 @@
+package kh.edu.rupp.ckcc.derkamsan;
+
+public class Profile  {
+   private String userName ;
+   private String email;
+   private String passWord;
+   private String phoneNumber;
+   private String id;
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassWord() {
+        return passWord;
+    }
+
+    public void setPassWord(String passWord) {
+        this.passWord = passWord;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
